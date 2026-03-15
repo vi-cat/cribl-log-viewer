aafaf90d3c2208d1b602087546ff6f0a85b3aa03
Vi <v@barecode.net>
2026-03-15 15:21:14 -0500

    feat: add row expand/collapse w formatted JSON detail

--- src/components/LogsTable/LogRow.tsx
+++ src/components/LogsTable/LogRow.tsx
@@ -1,13 +1,32 @@
+import { useState } from "react";
 import type { LogEntry } from "../../types/LogEntry";
 import { formatTime } from "../../utils/formatTime";
 import styles from "./LogsTable.module.css";
 
-export const LogRow = ({ log }: { log: LogEntry }) => {
+export const LogRow = ({ log, index }: { log: LogEntry; index: number }) => {
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <tr>
-      <td className={styles.toggleCell}>❯</td>
-      <td className={styles.timeCell}>{formatTime(log._time)}</td>
-      <td className={styles.eventCell}>{JSON.stringify(log)}</td>
-    </tr>
+    <>
+      <tr
+        className={`${styles.row}${index % 2 === 0 ? ` ${styles.rowEven}` : ""}`}
+        onClick={() => setExpanded((e) => !e)}
+      >
+        <td
+          className={`${styles.toggleCell}${expanded ? ` ${styles.toggleCellExpanded}` : ""}`}
+        >
+          ❯
+        </td>
+        <td className={styles.timeCell}>{formatTime(log._time)}</td>
+        <td className={styles.eventCell}>{JSON.stringify(log)}</td>
+      </tr>
+      {expanded && (
+        <tr className={styles.row} onClick={() => setExpanded(false)}>
+          <td colSpan={3} className={styles.expandedCell}>
+            <pre>{JSON.stringify(log, null, 2)}</pre>
+          </td>
+        </tr>
+      )}
+    </>
   );
 };
--- src/components/LogsTable/LogsTable.tsx
+++ src/components/LogsTable/LogsTable.tsx
@@ -8,24 +8,26 @@ type LogsTableProps = {
 
 export const LogsTable = ({ logs }: LogsTableProps) => {
   return (
-    <table className={styles.table}>
-      <colgroup>
-        <col style={{ width: "2rem" }} />
-        <col style={{ width: "14rem" }} />
-        <col />
-      </colgroup>
-      <thead>
-        <tr>
-          <th></th>
-          <th>Time</th>
-          <th>Event</th>
-        </tr>
-      </thead>
-      <tbody>
-        {logs.map((log, index) => (
-          <LogRow log={log} key={index} />
-        ))}
-      </tbody>
-    </table>
+    <div className={styles.contained}>
+      <table className={styles.table}>
+        <colgroup>
+          <col style={{ width: "2rem" }} />
+          <col style={{ width: "14rem" }} />
+          <col />
+        </colgroup>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Time</th>
+            <th>Event</th>
+          </tr>
+        </thead>
+        <tbody>
+          {logs.map((log, index) => (
+            <LogRow log={log} key={index} index={index} />
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
